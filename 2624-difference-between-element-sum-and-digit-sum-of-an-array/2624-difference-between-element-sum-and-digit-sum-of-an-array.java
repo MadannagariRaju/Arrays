322203cdf3aa00ab39a9190c2d4827c68dba999d
MADANNAGARI RAJU <106908371+MadannagariRaju@users.noreply.github.com>
2024-05-22 21:05:17 +0530

    Update 2624-difference-between-element-sum-and-digit-sum-of-an-array.java

--- 2624-difference-between-element-sum-and-digit-sum-of-an-array/2624-difference-between-element-sum-and-digit-sum-of-an-array.java
+++ 2624-difference-between-element-sum-and-digit-sum-of-an-array/2624-difference-between-element-sum-and-digit-sum-of-an-array.java
@@ -1,27 +1,51 @@
+// class Solution {
+//     public int differenceOfSum(int[] nums) {
+//         int n=nums.length;
+//         int sum=0;
+//         for(int i=0;i<n;i++) {
+//             sum+=nums[i];
+//         }
+//         // System.out.println(sum);
+//         int dSum=0;
+//         for(int i=0;i<n;i++) {
+//             if(nums[i]<10)
+//             {
+//                 dSum+=nums[i];
+//                 System.out.println(dSum);
+//             }
+//             else{
+//                 int m=nums[i];
+//                 while(m>0) {
+//                     dSum+=m%10;
+//                     m=m/10;
+//                 }
+//             }
+//         }
+//         // System.out.println(dSum);
+//         return sum-dSum;
+//     }
+// }
+
+
 class Solution {
     public int differenceOfSum(int[] nums) {
         int n=nums.length;
-        int sum=0;
+        int sum=0,dSum=0;
         for(int i=0;i<n;i++) {
             sum+=nums[i];
+            dSum+=digitSum(nums[i]);
         }
-        // System.out.println(sum);
-        int dSum=0;
-        for(int i=0;i<n;i++) {
-            if(nums[i]<10)
-            {
-                dSum+=nums[i];
-                System.out.println(dSum);
-            }
-            else{
-                int m=nums[i];
-                while(m>0) {
-                    dSum+=m%10;
-                    m=m/10;
-                }
-            }
-        }
-        // System.out.println(dSum);
         return sum-dSum;
+    }
+    public int digitSum(int n) {
+        int sum=0;
+        if(n<10) {
+            return n;
+        }
+        while(n>0) {
+            sum+=n%10;
+            n=n/10;
+        }
+        return sum;
     }
 }
